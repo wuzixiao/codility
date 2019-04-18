43b7d44c478d48ebe35ee0c573576a9a20639e7c
Tom Liu <wuzi.xiao@gmail.com>
2019-04-19 11:43:18 +1200

    commit from vscode

---
+++ ConsoleApp1/IteneraryManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Provides capabilities for managing a customers itinerary.
+    /// </summary>
+    public class ItineraryManager
+    {
+        private readonly IDataStore _dataStore;
+        private readonly IDistanceCalculator _distanceCalculator;
+
+        //TODO : Use DI and pass interface into contractor
+        public ItineraryManager()
+        {
+            _dataStore = new SqlAgentStore(ConfigurationManager.ConnectionStrings["SqlDbConnection"].ConnectionString);
+            _distanceCalculator = new GoogleMapsDistanceCalculator(ConfigurationManager.AppSettings["GoogleMapsApiKey"]);
+        }
+
+        /// <summary>
+        /// Calculates a quote for a customers itinerary from a provided list of airline providers.
+        /// </summary>
+        /// <param name="itineraryId">The identifier of the itinerary</param>
+        /// <param name="priceProviders">A collection of airline price providers.</param>
+        /// <returns>A collection of quotes from the different airlines.</returns>
+        //TODO: change it to be async method because there is async function used inside
+        public IEnumerable<Quote> CalculateAirlinePrices(int itineraryId, IEnumerable<IAirlinePriceProvider> priceProviders)
+        {
+            //TODO: check priceProviders is null 
+            //TODO: await the result of async method
+            //TODO : add try..catch 
+            var itinerary = _dataStore.GetItinaryAsync(itineraryId).Result; 
+            if (itinerary == null)
+                throw new InvalidOperationException();
+
+            List<Quote> results = new List<Quote>();
+            Parallel.ForEach(priceProviders, provider =>
+            {
+                var quotes = provider.GetQuotes(itinerary.TicketClass, itinerary.Waypoints);
+                //TODO : will quotes be null?
+                foreach (var quote in quotes)
+                    //TODO : it could crash as list doesn't support concurrency
+                    results.Add(quote);
+            });
+
+            return results;
+        }
+
+        /// <summary>
+        /// Calculates the total distance traveled across all waypoints in a customers itinerary.
+        /// </summary>
+        /// <param name="itineraryId">The identifier of the itinerary</param>
+        /// <returns>The total distance traveled.</returns>
+        public async Task<double> CalculateTotalTravelDistanceAsync(int itineraryId)
+        {
+            var itinerary = await _dataStore.GetItinaryAsync(itineraryId);
+            if (itinerary == null)
+                throw new InvalidOperationException();
+            double result = 0;
+            for (int i = 0; i < itinerary.Waypoints.Count - 1; i++)
+            {
+                //TODO : should use await
+                result = result + _distanceCalculator.GetDistanceAsync(itinerary.Waypoints[i],
+                     itinerary.Waypoints[i + 1]).Result;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Loads a Travel agents details from Storage
+        /// </summary>
+        /// <param name="id">The id of the travel agent.</param>
+        /// <param name="updatedPhoneNumber">If set updates the agents phone number.</param>
+        /// <returns>The travel agent if located, otherwise null.</returns>
+        public TravelAgent FindAgent(int id, string updatedPhoneNumber)
+        {
+            //TODO : is it async call?
+            var agentDao = _dataStore.GetAgent(id);
+            if (agentDao == null)
+                return null;
+            //TODO : IsNullOrEmpty, or use more strict roles to validate phone number
+            if (!string.IsNullOrWhiteSpace(updatedPhoneNumber))
+            {
+                agentDao.PhoneNumber = updatedPhoneNumber;
+                //TODO : is it async call?
+                _dataStore.UpdateAgent(id, agentDao);
+            }
+
+            return Mapper.Map<TravelAgent>(agentDao);
+        }
+    }
+}
---
+++ XUnitTestProject1/CouponManagerTest.cs
@@ -0,0 +1,170 @@
+﻿using ConsoleApp1;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class CouponManagerTest
+    {
+        private readonly ILogger logger;
+        private readonly ICouponProvider couponProvider;
+
+        [Fact]
+        public void Throw_Exception_When_Logger_Is_Null()
+        {
+            //Arrange
+            var nameOfLogger = nameof(logger);
+            var mockCouponProvider = new Mock<ICouponProvider>();
+
+            //Act and Assert
+            var exception = Assert.Throws<ArgumentNullException>(() => new CouponManager(null, mockCouponProvider.Object));
+            Assert.Equal(nameOfLogger,exception.ParamName);
+            exception = Assert.Throws<ArgumentNullException>(() => new CouponManager(null, null));
+            Assert.Equal(nameOfLogger,exception.ParamName);
+        }
+
+        [Fact]
+        public void Throw_Exception_When_CouponProvider_Is_Null()
+        {
+            //Arrange
+            var nameOfCouponProvider = nameof(couponProvider);
+            var mockLogger = new Mock<ILogger>();
+
+            //Act and Assert
+            var exception = Assert.Throws<ArgumentNullException>(() => new CouponManager(mockLogger.Object, null));
+            Assert.Equal(nameOfCouponProvider,exception.ParamName);
+        }
+
+        [Fact]
+        public async Task Throw_Exception_When_Evaluators_Null()
+        {
+            //Arrange
+            var mockLogger = new Mock<ILogger>();
+            var mockCouponProvider = new Mock<ICouponProvider>();
+            var couponId = new Guid();
+            var userId = new Guid();
+
+            var manager = new CouponManager(mockLogger.Object, mockCouponProvider.Object);
+            IEnumerable<Func<Coupon, Guid, bool>> evaluators = null;
+
+            //Act
+            var exception = await Assert.ThrowsAsync<ArgumentNullException>(() => manager.CanRedeemCoupon(couponId, userId, evaluators));
+
+            //Assert
+            Assert.Equal(nameof(evaluators),exception.ParamName);
+        }
+
+        [Fact]
+        public async Task Throw_Exception_When_Retrieved_Coupon_Null()
+        {
+            //Arrange
+            var couponId = new Guid();
+            var userId = new Guid();
+            var mockLogger = new Mock<ILogger>();
+            var mockEvaluators = new List<Func<Coupon, Guid, bool>>();
+            var mockCouponProvider = new Mock<ICouponProvider>();
+            mockCouponProvider
+                .Setup(provider => provider.Retrieve(couponId))
+                .ReturnsAsync(() => null);
+
+            var manager = new CouponManager(mockLogger.Object, mockCouponProvider.Object);
+
+            //Act Assert
+            await Assert.ThrowsAsync<KeyNotFoundException>(() => manager.CanRedeemCoupon(couponId, userId, mockEvaluators));
+        }
+
+        [Fact]
+        public async Task Return_True_When_Evaluators_Empty()
+        {
+            //Arrange
+            var couponId = new Guid();
+            var userId = new Guid();
+            var mockLogger = new Mock<ILogger>();
+            var mockEvaluators = new List<Func<Coupon, Guid, bool>>();
+
+            var mockCouponProvider = new Mock<ICouponProvider>();
+            mockCouponProvider
+                .Setup(provider => provider.Retrieve(couponId))
+                .ReturnsAsync(() => new Coupon());
+
+            var manager = new CouponManager(mockLogger.Object, mockCouponProvider.Object);
+
+            //Act
+            var result = await manager.CanRedeemCoupon(couponId, userId, mockEvaluators);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task Return_False_When_All_Evaluator_Fail()
+        {
+            //Arrange
+            var couponId = new Guid();
+            var userId = new Guid();
+            var mockLogger = new Mock<ILogger>();
+
+            bool Rfalse(Coupon coupon, Guid guid)
+            {
+                return false;
+            }
+            bool Rfalse2(Coupon coupon, Guid guid)
+            {
+                return false;
+            }
+            var mockEvaluators = new List<Func<Coupon, Guid, bool>>() { Rfalse, Rfalse2 };
+
+
+            var mockCouponProvider = new Mock<ICouponProvider>();
+            mockCouponProvider
+                .Setup(provider => provider.Retrieve(couponId))
+                .ReturnsAsync(() => new Coupon());
+
+            var manager = new CouponManager(mockLogger.Object, mockCouponProvider.Object);
+
+            //Act
+            var result = await manager.CanRedeemCoupon(couponId, userId, mockEvaluators);
+
+            //Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task Return_True_When_Any_Evaluator_Pass()
+        {
+            //Arrange
+            var couponId = new Guid();
+            var userId = new Guid();
+            var mockLogger = new Mock<ILogger>();
+
+            bool Rfalse(Coupon coupon, Guid guid)
+            {
+                return false;
+            }
+            bool Rtrue(Coupon coupon, Guid guid)
+            {
+                return true;
+            }
+            var mockEvaluators = new List<Func<Coupon, Guid, bool>>() { Rfalse, Rtrue };
+
+
+            var mockCouponProvider = new Mock<ICouponProvider>();
+            mockCouponProvider
+                .Setup(provider => provider.Retrieve(couponId))
+                .ReturnsAsync(() => new Coupon());
+
+            var manager = new CouponManager(mockLogger.Object, mockCouponProvider.Object);
+
+            //Act
+            var result = await manager.CanRedeemCoupon(couponId, userId, mockEvaluators);
+
+            //Assert
+            Assert.True(result);
+        }
+    }
+}
