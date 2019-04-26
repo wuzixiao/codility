0c31a5c5d3c9beb9250bb3493e1b2e6315e4d5f9
Tom Liu <wuzi.xiao@gmail.com>
2019-04-26 18:55:20 +1200

    a better find change point impl

--- ConsoleApp1/Codility.cs
+++ ConsoleApp1/Codility.cs
@@ -528,8 +528,29 @@ namespace ConsoleApp1
             There are several solutions to resolve this problem:
                 1. Iterate from the left side of the list, say i is current point. get the max of i's left.
                     use it compare with the right of i, if no element is smaller than i. It is the position.
-                2. Obviously, the 1st solution is too slow.
+                2. Obviously, the 1st solution is too slow because it is O(n*n). It can be optimized by using O(n) space
+                    to save the largest temperature of right of each element thus we don't need to iterate the inner loop.
          */
+         public int FindChangePoint_Better(int[] T) {
+             var maxLeft = Int32.MinValue;
+             var minRight = Int32.MaxValue;
+             var len = T.Length;
+             var minsRight = new int[len];
+             //build minsRight array from right to left
+             for(var i = T.Length-1; i >= 0; i--) {
+                 minsRight[i] = Math.Min(minsRight[i], minRight);
+                 minRight = minsRight[i];
+             }
+
+             for(var i = 0; i < T.Length -1; i++) {
+                 maxLeft = Math.Max(T[i], maxLeft);
+                 if(maxLeft == minsRight[i]) {
+                     return i;
+                 }
+             }
+
+             return -1;
+         }
          public int FindChangePoint(int[] T) 
          {
              var maxLeft = Int32.MinValue;
