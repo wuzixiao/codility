116439d0bccaaf26e85ab265d5c8ced86171e969
Tom Liu <wuzi.xiao@gmail.com>
2019-05-03 09:28:39 +1200

    better count passing cars

--- ConsoleApp1/Codility.cs
+++ ConsoleApp1/Codility.cs
@@ -794,6 +794,31 @@ namespace ConsoleApp1
             }
             return nums > 1000000000 ? -1 : nums;
         }
+
+        public int CountPassingCars2(int[] A)
+        {
+            int count = 0;
+            int multiply = 0;
+            foreach (int car in A)
+            {
+                if (car == 0)
+                {
+                    multiply = multiply + 1;
+                }
+                if (multiply > 0)
+                {
+                    if (car == 1)
+                    {
+                        count = count + multiply;
+                        if (count > 1000000000)
+                        {
+                            return -1;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
     }
 
     public class CountingElement
