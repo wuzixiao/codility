1ac1eb99100ca40d1b381d522227de63a2f3142f
Tom Liu <wuzi.xiao@gmail.com>
2019-06-09 15:02:56 +1200

    equileader

--- ConsoleApp1/Codility.cs
+++ ConsoleApp1/Codility.cs
@@ -573,7 +573,48 @@ namespace ConsoleApp1
 
     public class Leader
     {
-        public int EquiLeader(int[] A) {
+        public int Equileader(int[] A) {
+            int n = A.Length;
+            int size =0;
+            Stack<int> s = new Stack<int>();
+            
+            for (int i=0; i<n; i++)
+            {
+                if(size ==0)
+                {
+                    size +=1;
+                    s.Push(A[i]);
+                }
+                else
+                {
+                    if (s.Peek() != A[i]) size -=1;
+                    else size +=1;
+                }           
+            }
+            int candidate = -1;
+            if (size>0) candidate = s.Peek();
+            int count =0;
+            int leader= -1;
+            
+            for (int i=0; i<n; i++)
+            {
+            if (A[i] == candidate) count +=1;
+            if (count > n/2) leader = candidate;
+            }
+            
+            int equiLeaders=0;
+            int leaders=0;
+            
+            for (int i=0; i<n; i++)
+            {
+                if (A[i] == leader) leaders++;
+                if (leaders >(i+1)/2 && count-leaders >(n-1-i)/2) equiLeaders++;             
+            }        
+            
+            return equiLeaders;
+        }
+
+        public int EquiLeader_stupid(int[] A) {
             var ret = 0;
             for(var i = 1; i < A.Length -1; i++) {
                 var d1 = Dominator(A.Take(i).ToArray());
